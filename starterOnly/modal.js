de7975b2456b9a01a9e77f0bc01a9c09026a93c2
samiNedjai <sami.nedjai@outlook.fr>
2024-02-10 17:36:55 +0100

    ajout les validation_2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,104 +27,178 @@ function launchModal() {
 }
 
 // Validate form on submit
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault(); // Prevent form submission
-  if(validateForm()) {
+  if (validateForm()) {
     // Si la validation est réussie, afficher le message de remerciement
     showModalThankYou();
   }
 });
+// Validate form en Real-time
+document.getElementById("first").addEventListener("input", validateFirstName);
+document.getElementById("last").addEventListener("input", validateLastName);
+document.getElementById("email").addEventListener("input", validateEmail);
+document
+  .getElementById("birthdate")
+  .addEventListener("input", validateBirthdate);
+document
+  .getElementById("quantity")
+  .addEventListener("input", validateTournaments);
 
-// Validate form en Real-time 
-function validateForm() {
-    let isValid = true; // Initialiser comme valide
-    const today = new Date();
-    const minimumAge = 18;
-    const firstName = document.getElementById("first");
-    const lastName = document.getElementById("last");
-    const email = document.getElementById("email");
-    const birthdate = document.getElementById("birthdate");
-    const locations = document.querySelectorAll('input[name="location"]');
-    const termsAccepted = document.getElementById("checkbox1").checked;
+// Pour chaque input de type radio pour la ville, ajoutez un écouteur.
+// Cela suppose que vous avez un groupe de boutons radio avec le nom "location".
+const locationRadios = document.querySelectorAll('input[name="location"]');
+locationRadios.forEach((radio) => {
+  radio.addEventListener("change", validateCitySelection);
+});
 
-    // Validation pour firstName et lastName (doivent contenir au moins 2 caractères et pas de chiffres)
-    [firstName, lastName].forEach(field => {
-        if (field.value.length < 2 || /\d/.test(field.value)) {
-            displayError(field, "Doit contenir au moins 2 caractères et pas de chiffres.");
-            isValid = false;
-        } else {
-            clearError(field);
-        }
-    });
+document.getElementById("checkbox1").addEventListener("change", validateTerms);
 
-    // Validation pour email (doit respecter le format d'une adresse email)
-    if (!/\S+@\S+\.\S+/.test(email.value)) {
-        displayError(email, "Doit être une adresse email valide.");
-        isValid = false;
-    } else {
-        clearError(email);
-    }
-
-    // Validation pour la date de naissance
-    if (birthdate.value === "") {
-      // Si l'utilisateur n'a pas sélectionné de date
-      displayError(birthdate, "Veuillez sélectionner une date.");
-      isValid = false;
+// Validate first name
+function validateFirstName() {
+  const firstName = document.getElementById("first");
+  if (firstName.value.length < 2 || /\d/.test(firstName.value)) {
+    displayError(
+      firstName,
+      "Prénom doit contenir au moins 2 caractères et pas de chiffres."
+    );
+    return false;
   } else {
-      let birthDate = new Date(birthdate.value);
-      let age = today.getFullYear() - birthDate.getFullYear();
-      let m = today.getMonth() - birthDate.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-          age--;
-      }
-      if (age < minimumAge) {
-          // Si l'utilisateur a moins de 18 ans
-          displayError(birthdate, `Vous devez avoir plus de ${minimumAge} ans.`);
-          isValid = false;
-      } else {
-          clearError(birthdate);
-      }
+    clearError(firstName);
+    return true;
   }
+}
 
-    // Validation pour la sélection d'une ville (au moins une ville doit être sélectionnée)
-    if (locations.length === 0) {
-        displayError(document.querySelector(".text-label"), "Vous devez sélectionner une ville.", true);
-        isValid = false;
-    } else {
-        clearError(document.querySelector(".text-label"), true);
+// Validate last name
+function validateLastName() {
+  const lastName = document.getElementById("last");
+  if (lastName.value.length < 2 || /\d/.test(lastName.value)) {
+    displayError(
+      lastName,
+      "Nom doit contenir au moins 2 caractères et pas de chiffres."
+    );
+    return false;
+  } else {
+    clearError(lastName);
+    return true;
+  }
+}
+
+// Validate email
+function validateEmail() {
+  const email = document.getElementById("email");
+  if (!/\S+@\S+\.\S+/.test(email.value)) {
+    displayError(email, "Doit être une adresse email valide.");
+    return false;
+  } else {
+    clearError(email);
+    return true;
+  }
+}
+
+// Validate Birthdate
+function validateBirthdate() {
+  const birthdate = document.getElementById("birthdate");
+  const today = new Date();
+  const minimumAge = 18;
+  if (birthdate.value === "") {
+    displayError(birthdate, "Veuillez sélectionner une date.");
+    return false;
+  } else {
+    let birthDate = new Date(birthdate.value);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
     }
-
-    // Validation des conditions d'utilisation (doit être cochée)
-    if (!termsAccepted) {
-        displayError(document.getElementById("checkbox1"), "Vous devez accepter les conditions d'utilisation.", true);
-        isValid = false;
+    if (age < minimumAge) {
+      displayError(birthdate, `Vous devez avoir plus de ${minimumAge} ans.`);
+      return false;
     } else {
-        clearError(document.getElementById("checkbox1"), true);
+      clearError(birthdate);
+      return true;
     }
+  }
+}
+//  Validate nombre de tournois
+function validateTournaments() {
+  const tournaments = document.getElementById("quantity");
+  if (!tournaments.value || isNaN(tournaments.value) || tournaments.value < 0) {
+    displayError(tournaments, "Veuillez entrer un nombre valide de tournois.");
+    return false;
+  } else {
+    clearError(tournaments);
+    return true;
+  }
+}
+// Validation de la sélection d'une ville
+function validateCitySelection() {
+  const locations = document.querySelectorAll('input[name="location"]:checked');
+  if (locations.length === 0) {
+    displayError(
+      document.querySelector(".text-label"),
+      "Vous devez sélectionner une ville.",
+      true
+    );
+    return false;
+  } else {
+    clearError(document.querySelector(".text-label"), true);
+    return true;
+  }
+}
+// Validation de l'acceptation des conditions d'utilisation
+function validateTerms() {
+  const termsAccepted = document.getElementById("checkbox1").checked;
+  if (!termsAccepted) {
+    displayError(
+      document.getElementById("checkbox1"),
+      "Vous devez accepter les conditions d'utilisation.",
+      true
+    );
+    return false;
+  } else {
+    clearError(document.getElementById("checkbox1"), true);
+    return true;
+  }
+}
 
-    return isValid;
+// Validate form (including other validations)
+function validateForm() {
+  // Utilisez un opérateur logique AND pour combiner les validations
+  const isFormValid =
+    validateFirstName() &&
+    validateLastName() &&
+    validateEmail() &&
+    validateBirthdate() &&
+    validateTournaments() &&
+    validateCitySelection() &&
+    validateTerms();
+  return isFormValid;
 }
 
 // Afficher les messages d'erreur
 function displayError(input, message, isCheckboxOrRadio = false) {
-    const parent = isCheckboxOrRadio ? input.parentNode.parentNode : input.parentElement; // Parent correct pour les checkboxes/radios
-    let error = parent.querySelector(".error-message");
-    if (!error) {
-        error = document.createElement("div");
-        error.className = "error-message";
-        parent.appendChild(error);
-    }
-     error.textContent = message;
-    
+  const parent = isCheckboxOrRadio
+    ? input.parentNode.parentNode
+    : input.parentElement; // Parent correct pour les checkboxes/radios
+  let error = parent.querySelector(".error-message");
+  if (!error) {
+    error = document.createElement("div");
+    error.className = "error-message";
+    parent.appendChild(error);
+  }
+  error.textContent = message;
 }
 
 // Effacer les messages d'erreur
 function clearError(input, isCheckboxOrRadio = false) {
-    const parent = isCheckboxOrRadio ? input.parentNode.parentNode : input.parentElement; // Parent correct pour les checkboxes/radios
-    const error = parent.querySelector(".error-message");
-    if (error) {
-        parent.removeChild(error);
-    }
+  const parent = isCheckboxOrRadio
+    ? input.parentNode.parentNode
+    : input.parentElement; // Parent correct pour les checkboxes/radios
+  const error = parent.querySelector(".error-message");
+  if (error) {
+    parent.removeChild(error);
+  }
 }
 
 // Afficher le modal de remerciement et ajouter un bouton pour fermer le modal
@@ -136,25 +210,20 @@ function showModalThankYou() {
           <span class='close' id='closeModal'></span>
           <div class='modal-body'>
               <h2>Merci pour votre inscription!</h2>
-              <button class='btn-submit' id='closeThanks'>Fermer</button>
+              <button class='btn-submit ' id='closeThanks'>Fermer</button>
           </div>
       </div>`;
 
   // Sélectionner les nouveaux éléments pour fermer le modal
-  const closeModalButton = document.getElementById('closeThanks');
-  const closeModalCross = document.getElementById('closeModal');
- // Assurez-vous que cette ligne est présente pour capturer l'élément close
-const closeBtn = document.getElementById('closebtn'); // Capture le bouton de fermeture
+  const closeModalButton = document.getElementById("closeThanks");
+  const closeModalCross = document.getElementById("closeModal");
 
-  // Ajoutez un écouteur d'événements pour fermer le modal lorsque la croix est cliquée
-  closeBtn.addEventListener("click", closeModal);
+  // const closeBtn = document.getElementById('closebtn'); // Capture le bouton de fermeture
+  // closeBtn.addEventListener("click", closeModal);
 
   // Ajouter l'événement onclick pour fermer le modal avec le bouton
-  closeModalButton.addEventListener('click', closeModal);
+  closeModalButton.addEventListener("click", closeModal);
 
   // Ajouter l'événement onclick pour fermer le modal avec la croix (✕)
-  closeModalCross.addEventListener('click', closeModal);
+  closeModalCross.addEventListener("click", closeModal);
 }
-
-
-
